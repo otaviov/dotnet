826409875a719269ac4e708c25227c99d07a484a
otaviov <amandavv32@gmail.com>
2021-04-27 12:37:37 -0300

    create a console application

---
+++ CursoDotnet/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CursoDotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int  numeroDeVezes = 10;
+
+            for(int i =0; i < numeroDeVezes; i++){
+                Console.WriteLine($"Bem-Vindo ao curso de .Net {i}");
+            }
+        }
+    }
+}
