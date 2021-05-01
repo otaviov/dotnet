8fda525d9673f90c57560edbb999cb3280e21032
otaviov <amandavv32@gmail.com>
2021-04-30 22:55:27 -0300

    add aluno. Lista de aluno

--- Revisao/Program.cs
+++ Revisao/Program.cs
@@ -6,6 +6,63 @@ namespace Revisao
     {
         static void Main(string[] args)
         {
+            Aluno[] alunos = new Aluno[5];
+            int indiceAluno = 0;
+
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario != "0")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        //TODO: Adicionar aluno
+                        Console.WriteLine("Informe o nome do aluno: ");
+                        var aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+                        
+                        Console.WriteLine("Informe  nota do aluno:");
+
+                        if (decimal.TryParse(Console.ReadLine(), out decimal nota)){
+                            aluno.Nota = nota;
+                        }
+                        else{
+                            throw new ArithmeticException("Valor da nota deve ser decimal");
+                        }
+                        
+                        alunos[indiceAluno] = aluno;
+                        indiceAluno++;
+                        Console.WriteLine();
+                        break;
+
+                    case "2":
+                        //TODO: Listar alunos
+                        foreach(var listaalunos in alunos){
+
+                            if(string.IsNullOrEmpty(listaalunos.Nome)){
+                            Console.WriteLine($"ALUNO: {listaalunos.Nome} - NOTA: {listaalunos.Nota}");
+                            }
+                            
+                        }
+
+                        Console.WriteLine();
+                        break;
+
+                    case "3":
+                        //TODO: Calcular média geral
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException("Comando inválido!");
+                }
+
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
             Console.WriteLine("Informe a opção desejada: ");
             Console.WriteLine("1 - Inserir novo aluno");
             Console.WriteLine("2 - Listar Alunos");
@@ -14,33 +71,8 @@ namespace Revisao
             Console.WriteLine();
 
             string opcaoUsuario = Console.ReadLine();
-
-            while (opcaoUsuario != "0"){
-                switch(opcaoUsuario){
-                    case "1":
-                        //TODO: Adicionar aluno
-                        break;
-                    case "2":
-                        //TODO: Listar alunos
-                        break;
-                    case "3":
-                        //TODO: Calcular média geral
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException("Comando inválido!");
-                }
-
-            Console.WriteLine("Informe a opção desejada: ");
-            Console.WriteLine("1 - Inserir novo aluno");
-            Console.WriteLine("2 - Listar Alunos");
-            Console.WriteLine("3 - Calcular média geral");
-            Console.WriteLine("0 - Sair");
             Console.WriteLine();
-
-            opcaoUsuario = Console.ReadLine();
-
-            }
-            
+            return opcaoUsuario;
         }
     }
 }
