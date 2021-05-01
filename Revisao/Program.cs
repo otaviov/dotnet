ee101981edc58d5dc21ad93573ea725513ca9e39
otaviov <amandavv32@gmail.com>
2021-04-30 22:00:05 -0300

    Revisão Intrdutória

---
+++ Revisao/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a opção desejada: ");
+            Console.WriteLine("1 - Inserir novo aluno");
+            Console.WriteLine("2 - Listar Alunos");
+            Console.WriteLine("3 - Calcular média geral");
+            Console.WriteLine("0 - Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+
+            while (opcaoUsuario != "0"){
+                switch(opcaoUsuario){
+                    case "1":
+                        //TODO: Adicionar aluno
+                        break;
+                    case "2":
+                        //TODO: Listar alunos
+                        break;
+                    case "3":
+                        //TODO: Calcular média geral
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException("Comando inválido!");
+                }
+
+            Console.WriteLine("Informe a opção desejada: ");
+            Console.WriteLine("1 - Inserir novo aluno");
+            Console.WriteLine("2 - Listar Alunos");
+            Console.WriteLine("3 - Calcular média geral");
+            Console.WriteLine("0 - Sair");
+            Console.WriteLine();
+
+            opcaoUsuario = Console.ReadLine();
+
+            }
+            
+        }
+    }
+}
